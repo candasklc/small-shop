4fb70b9c2b760b72bcb2d93754ead20e64aa5cb8
Candaş Kılıç <63592580+candasklc@users.noreply.github.com>
2022-03-18 20:33:14 +0100

    dialog componet is created.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatListModule } from '@angular/material/list';
+
 import { AppComponent } from './app.component';
 import { MainPageComponent } from './components/main-page/main-page.component';
 import { ProductCardComponent } from './components/product-card/product-card.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { ProductShowCaseComponent } from './components/product-show-case/product-show-case.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatCardModule } from '@angular/material/card';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
-import { MatTableModule } from '@angular/material/table';
-import { MatMenuModule } from '@angular/material/menu';
 import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
-import { MatListModule } from '@angular/material/list';
 import { FavouritesComponent } from './components/favourites/favourites.component';
 import { CalculatorComponent } from './components/calculator/calculator.component';
+import { DialogComponent } from './components/dialog/dialog.component';
 
 @NgModule({
   declarations: [
@@ -29,7 +32,8 @@ import { CalculatorComponent } from './components/calculator/calculator.componen
     ProductShowCaseComponent,
     ShoppingCartComponent,
     FavouritesComponent,
-    CalculatorComponent
+    CalculatorComponent,
+    DialogComponent
   ],
   imports: [
     BrowserModule,
@@ -41,6 +45,7 @@ import { CalculatorComponent } from './components/calculator/calculator.componen
     MatMenuModule,
     MatListModule,
     MatTableModule,
+    MatDialogModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogComponent } from 'src/app/components/dialog/dialog.component';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(public dialog: MatDialog) { }
 
   ngOnInit(): void {
+  }
+
+  public helpDialog(): void {
+    this.dialog.open(DialogComponent);
   }
 
 }
